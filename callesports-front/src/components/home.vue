65200584556f8ea429045f37d1ea89a303e5fd5c
JOFEGE-BR\guilherme.montico <guilhermebmontico@gmail.com>
2021-01-25 17:49:42 -0300

    adding components

---
+++ callesports-front/src/components/home.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <section class="home">
+    <topbarmenu />
+    <h1>home Components</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+  import topbarmenu from './topbarmenu'
+
+  export default  {
+    name: 'home',
+    components:{
+      topbarmenu
+    },
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .home {
+
+  }
+</style>
---
+++ callesports-front/src/components/topbarmenu.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+
+  <section class="topbarmenu">
+    <div class="div-topbarmenu">
+      <h1>topbar-menu Component</h1>
+    </div>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'topbarmenu',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .topbarmenu {
+
+  }
+  .div-topbarmenu{
+    background-color: black;
+    margin-top: 0%;
+    padding-top: 0%;
+  }
+</style>
